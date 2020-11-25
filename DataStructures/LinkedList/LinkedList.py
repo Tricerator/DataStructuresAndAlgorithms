f1ea9a7376df3c5c4e9f301a042d2152562556f1
Tricerator <37005796+Tricerator@users.noreply.github.com>
2020-11-25 22:52:51 +0100

    Add files via upload

---
+++ DataStructures/LinkedList/LinkedList.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.descendant = None
+
+
+class LinkedList(object):
+
+    def __init__(self, value=None):
+
+        if value is None:
+            self.root = None
+            return
+        if type(value).__name__ == 'list':
+            if len(value) == 0:
+                self.root = None
+            else:
+                self.root = Node(value[0])
+                for i in value[1:]:
+                    self.Add(i)
+
+
+        else:
+            self.root = Node(value)
+
+    def AddToBeginning(self, value):
+        node = Node(value)
+        node.descendant = self.root
+        self.root = node
+
+    def Add(self, value):
+        node = self.root
+        while node.descendant is not None:
+            node = node.descendant
+        node.descendant = Node(value)
+
+    def PrintList(self):
+        print(self.createList())
+
+    def createList(self):
+        myList = []
+        myNode = self.root
+
+        while myNode is not None:
+            myList.append(myNode.value)
+            myNode = myNode.descendant
+        return myList
+
+    def Search(self, value):
+        myNode = self.root
+
+        while myNode is not None:
+            if myNode.value == value:
+                return True
+            myNode = myNode.descendant
+        return False
+
+    def RemoveValue(self, value):
+        node = self.root
+        if node.value == value:
+            self.root = self.root.descendant
+            return True
+        previousNode = node
+        node = node.descendant
+        while node is not None:
+            if node.value == value:
+                previousNode.descendant = node.descendant
+                return True
+            previousNode = node
+            node = node.descendant
+        return False
+
+    def RemoveAllPositionsOfValue(self, value):
+        a = True
+        while a:
+            a = self.RemoveValue(value)
+
+
+a = [16, 5, 32, 98, 12, 12, 12, 12, 12, 94, 91, 20, 22, 50, 47, 44, 2, 1, 28, 86, 66, 96, 85, 92]
+myList = LinkedList(a)
+myList.PrintList()
+myList.RemoveAllPositionsOfValue(12)
+myList.PrintList()
+myList.AddToBeginning(0)
+myList.PrintList()
+
