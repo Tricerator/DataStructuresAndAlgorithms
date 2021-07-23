37544f704227cc26edf4f3664d587b53580b39b6
michaleon <tricerator@DESKTOP-PMO4EPL.localdomain>
2021-07-23 23:51:12 +0200

    Added BubbleSort

---
+++ Algorithms/SortingAlgorithms/BubbleSort/bubbleSort.py
@@ -0,0 +1,17 @@
+
+def bubbleSort(array):
+   for j in range(len(array)-1):
+      change = False
+      for i in range(len(array)-1):
+          if array[i] > array[i+1]:
+              temp = array[i]
+              array[i] = array[i+1]
+              array[i+1] = temp
+              change = True
+      print(array)
+      if not change: return array    
+   return array
+
+arr = [1,3,4,4,3,3,2,2,1,1,1,1,1,0]
+arr2 = bubbleSort(arr)
+print(arr2)
